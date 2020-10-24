522879401b21e7e0211b5dc1504ac683906a62f6
unknown <luischichandagarcia@gmail.com>
2020-10-23 21:38:29 -0500

    Luis Modificacion

--- src/app/pages/dash/dash.page.ts
+++ src/app/pages/dash/dash.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-dash',
@@ -7,12 +8,168 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashPage implements OnInit {
 
+  private investmentChart: Chart;
+
   constructor() { }
 
   ngOnInit() {
-
+    this.generateCharts();
+    this.generateChartsLine();
+    this.generateChartsPie();
+    this.generateChartsRadar();
   }
 
+  generateCharts(){
+    const ctx = document.getElementById('investments-chart');
+    this.investmentChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['crusigrama', 'unir linea', 'complete', 'ahorcado', 'debates', 'foros'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+  }
 
+  generateChartsLine(){
+    const ctx = document.getElementById('investments-chart-line');
+    this.investmentChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['crusigrama', 'unir linea', 'complete', 'ahorcado', 'debates', 'foros'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+  }
 
+  generateChartsRadar(){
+    const ctx = document.getElementById('investments-chart-radar');
+    this.investmentChart = new Chart(ctx, {
+        type: 'radar',
+        data: {
+            labels: ['crusigrama', 'unir linea', 'complete', 'ahorcado', 'debates', 'foros'],
+            datasets: [{
+                label: 'Destresa Personal',
+                data: [15, 14, 17, 19, 20, 11],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scale: {
+                angleLines: {
+                    display: false
+                },
+                ticks: {
+                    suggestedMin: 1,
+                    suggestedMax: 20
+                }
+            }
+        }
+    });
+  }
+
+  generateChartsPie(){
+    const ctx = document.getElementById('investments-chart-pie2');
+    this.investmentChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ['crusigrama', 'unir linea', 'complete', 'ahorcado', 'debates', 'foros'],
+            datasets: [{
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+        }
+    });
+  }
 }
